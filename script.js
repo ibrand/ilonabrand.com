87da8268c69def1cac271c8c0453d40892b37ec1
Ilona Brand <theilonabrand@gmail.com>
2019-02-21 23:32:56 -0800

    better anchor tags and mobile responsiveness, also added artist statement

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Bind to the click of all links with a #hash in the href
-$('a[href^="#"]').click(function(e) {
+$('a[href^="#"]').click(function (e) {
   // Prevent the jump and the #hash from appearing on the address bar
-  e.preventDefault();
+  e.preventDefault()
   // Scroll the window, stop any previous animation, stop on user manual scroll
   // Check https://github.com/flesler/jquery.scrollTo for more customizability
   $(window)
     .stop(true)
-    .scrollTo(this.hash, { duration: 1000, interrupt: true });
-});
+    .scrollTo(this.hash, { duration: 1000, interrupt: true })
+
+  window.location.hash = this.hash
+})
